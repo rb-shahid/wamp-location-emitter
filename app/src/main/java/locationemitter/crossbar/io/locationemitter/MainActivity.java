56c120cec624042210aca33247cd96f8f8f14535
Omer Akram <om26er@gmail.com>
2018-12-10 20:27:13 +0500

    Add status message inside app

--- app/src/main/java/locationemitter/crossbar/io/locationemitter/MainActivity.java
+++ app/src/main/java/locationemitter/crossbar/io/locationemitter/MainActivity.java
@@ -1,36 +1,47 @@
 package locationemitter.crossbar.io.locationemitter;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.annotation.SuppressLint;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.SettingsClient;
+
+import io.crossbar.autobahn.wamp.Client;
+import io.crossbar.autobahn.wamp.Session;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOCATION_PERM_REQ_CODE = 1;
 
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationRequest mLocationRequest;
     private LocationCallback mLocationCallback;
 
+    private Session mWAMPSession;
+    private TextView mStatusText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        mStatusText = findViewById(R.id.status_text);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
         createLocationCallback();
         createLocationRequest();
     }
@@ -38,29 +49,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        checkLocationPersmissionIfNotPresentThenRequest();
-//        startLocationUpdates();
+        connectToServerAndPublishLocation();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-//        stopLocationUpdates();
+        stopLocationUpdates();
+    }
+
+    private void connectToServerAndPublishLocation() {
+        mWAMPSession = new Session();
+        mStatusText.setText("Connecting...");
+        mWAMPSession.addOnJoinListener((session, details) -> {
+            mStatusText.setText("Connected");
+            startLocationUpdates();
+        });
+        Client wampClient = new Client(mWAMPSession, "ws://192.168.100.42:8080/ws", "realm1");
+        wampClient.connect().whenComplete((exitInfo, throwable) -> stopLocationUpdates());
     }
 
     private void createLocationRequest() {
         mLocationRequest = new LocationRequest();
-
-        // Sets the desired interval for active location updates. This interval is
-        // inexact. You may not receive updates at all if no location sources are available, or
-        // you may receive them slower than requested. You may also receive updates faster than
-        // requested if other applications are requesting location at a faster interval.
-        mLocationRequest.setInterval(1000);
-
-        // Sets the fastest rate for active location updates. This interval is exact, and your
-        // application will never receive updates faster than this value.
-        mLocationRequest.setFastestInterval(1000);
-
+        mLocationRequest.setInterval(500);
+        mLocationRequest.setFastestInterval(500);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
@@ -70,75 +82,39 @@ public class MainActivity extends AppCompatActivity {
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 Location location = locationResult.getLastLocation();
-                Toast.makeText(getApplicationContext(), location.toString(), Toast.LENGTH_SHORT).show();
-                System.out.println(location);
+                if (location != null) {
+                    double lon = location.getLongitude();
+                    double lat = location.getLatitude();
+                    float speed = location.getSpeed();
+                    mWAMPSession.publish("io.crossbar.location", lon, lat, speed);
+                }
             }
         };
     }
 
     private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION},
+                    LOCATION_PERM_REQ_CODE);
+        } else {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
         }
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
     }
 
     private void stopLocationUpdates() {
-        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
-    }
-
-    private void checkLocationPersmissionIfNotPresentThenRequest() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    1);
-        } else {
-            // permission granted
-            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-            // TODO: 10/12/2018 do location related work
+        if (mWAMPSession.isConnected()) {
+            mWAMPSession.leave();
         }
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        mStatusText.setText("Disconnected");
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(
-                            this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                            && ActivityCompat.checkSelfPermission(this,
-                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
-                    }
-                    // TODO: 10/12/2018 user location related work
-
-                } else {
-                    // if permission not granted, it will close the app
-                    System.exit(0);
-                }
+        if (requestCode == LOCATION_PERM_REQ_CODE && grantResults.length > 0 && grantResults[0] == PERMISSION_GRANTED) {
+            startLocationUpdates();
         }
     }
 }
